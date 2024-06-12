a0d03d505f6c4941258109539f0063e8a64161da
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-06-13 08:39:42 +0900

    Feat: 게시글 작성 -> 게시글 DAO 생성자, 커밋, 롤백, sqlSession 닫기, 게시글 저장, ID로 찾기 기능 구현
    게시글 DAO 자동 커밋 생성자, 수동 커밋 생성자 구현
    커밋, 롤백, sqlSession 닫는 기능 구현
    ID로 DB에서 게시글 찾는 기능 구현
    DB에 게시글 저장 기능 구현

--- src/main/java/com/ohboon/ohboon/dao/BoardDAO.java
+++ src/main/java/com/ohboon/ohboon/dao/BoardDAO.java
@@ -1,4 +1,42 @@
 package com.ohboon.ohboon.dao;
 
+import java.util.Optional;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.ohboon.ohboon.dto.BoardDTO;
+import com.ohboon.ohboon.util.MybatisConnectionFactory;
+
 public class BoardDAO {
+
+	private final SqlSession sqlSession;
+
+	public BoardDAO() {
+		this.sqlSession = MybatisConnectionFactory.getSqlSession();
+	}
+
+	public BoardDAO(boolean isCommit) {
+		this.sqlSession = MybatisConnectionFactory.getSqlSession(isCommit);
+	}
+
+	public Optional<BoardDTO> findByID(long id) {
+		return Optional.ofNullable(sqlSession.selectOne("findByID", id));
+	}
+
+	public int save(BoardDTO boardDTO) {
+		return this.sqlSession.insert("save", boardDTO);
+	}
+
+	public void close() {
+		sqlSession.close();
+	}
+
+	public void rollback() {
+		sqlSession.rollback();
+	}
+
+	public void commit() {
+		sqlSession.commit();
+	}
+
 }
