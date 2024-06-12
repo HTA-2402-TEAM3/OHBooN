f4b11c55549a875faf877661a80d855d91e0c0a4
HMYOON <yhm1992@naver.com>
2024-06-12 14:58:27 +0900

    feat : Grade 매서드에서 불필요한 주석 제거

--- src/main/java/com/jhta2402/jhta2402_3_team_project_1/dto/Grade.java
+++ src/main/java/com/jhta2402/jhta2402_3_team_project_1/dto/Grade.java
@@ -6,18 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Grade {
-    // 열거형 제한: getter, 속성을 가지는 생성자 하나를 만들면 됨.
     MEMBER("member"),
     MANAGER("manager"),
     ADMIN("admin");
 
     private final String label;
-
-    /*public String label() {
-        return label;
-    }*/
-
-    /*Grade(String label) {
-        this.label = label;
-    }*/
 }
