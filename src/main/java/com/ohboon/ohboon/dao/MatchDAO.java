162a9437af5061d7cac21187d22f45070bd1033d
keyy1315 <lovelina1315@gmail.com>
2024-06-13 17:30:48 +0900

    feat: 매치 테이블 구현
    채팅방 생성함과 동시에 matching table row 저장

---
+++ src/main/java/com/ohboon/ohboon/dao/MatchDAO.java
@@ -0,0 +1,30 @@
+package com.ohboon.ohboon.dao;
+
+import com.ohboon.ohboon.dto.MatchDTO;
+import org.apache.ibatis.session.SqlSession;
+import util.MybatisConnectionFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MatchDAO {
+    private SqlSession ss = MybatisConnectionFactory.getSqlSession();
+
+    public long createMatch(long board_id, String boardWriterName) {
+        long match_id = 0;
+        Map<String, Object> map = new HashMap<>();
+        map.put("board_id", board_id);
+        map.put("boardWriterName", boardWriterName);
+        System.out.println(map);
+        int rs = ss.insert("createMatch", map);
+        ss.commit();
+
+        if(rs > 0 ){
+            match_id = ss.selectOne("getMatchId", map);
+        }
+        ss.commit();
+        ss.close();
+        return match_id;
+    }
+
+}
---
+++ src/main/java/com/ohboon/ohboon/service/MatchService.java
@@ -0,0 +1,10 @@
+package com.ohboon.ohboon.service;
+
+import com.ohboon.ohboon.dao.MatchDAO;
+
+public class MatchService {
+    public long getMatchId(long board_id, String boardWriterName) {
+        MatchDAO matchDAO = new MatchDAO();
+        return matchDAO.createMatch(board_id, boardWriterName);
+    }
+}
