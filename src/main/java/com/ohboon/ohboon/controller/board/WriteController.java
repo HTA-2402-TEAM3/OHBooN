e30b975e66d7f51b57b1d29c7b0a61bfaa56a4df
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-06-13 11:11:42 +0900

    Feat: 게시글 작성, 수정 -> 게시글 번호 유무에 따라 게시글 작성이나 수정을 수행하는 기능 구현
    게시글 번호 변수가 없으면 게시글 작성 기능을 수행하고, 변수가 있으면 수정 기능을 수행하는 기능 구현

--- src/main/java/com/ohboon/ohboon/controller/board/WriteController.java
+++ src/main/java/com/ohboon/ohboon/controller/board/WriteController.java
@@ -1,7 +1,10 @@
 package com.ohboon.ohboon.controller.board;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 import com.ohboon.ohboon.dto.BoardDTO;
 import com.ohboon.ohboon.service.BoardService;
@@ -19,20 +22,16 @@ public class WriteController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		HttpSession session = req.getSession();
-		session.setAttribute("loginEmail", "aa@aa.com");
-
-		Optional<String> inputBoardID = Optional.ofNullable(req.getParameter("boardID"));
+		String inputBoardID = req.getParameter("boardID");
 
 		BoardService boardService = new BoardService();
 
-		if (inputBoardID.isPresent()) {
-			long boardID = Long.parseLong(inputBoardID.get());
+		if (Objects.nonNull(inputBoardID)) {
+			long boardID = Long.parseLong(inputBoardID);
 
 			try {
 				BoardDTO boardDTO = boardService.read(boardID);
 				req.setAttribute("board", boardDTO);
-				System.out.println(boardDTO);
 
 			} catch (IllegalStateException illegalStateException) {
 				resp.sendError(HttpServletResponse.SC_NOT_FOUND, illegalStateException.getMessage());
@@ -40,5 +39,59 @@ public class WriteController extends HttpServlet {
 		}
 
 		req.getRequestDispatcher("/WEB-INF/board/write.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		HttpSession session = req.getSession();
+		String userEmail = (String)session.getAttribute("loginEmail");
+
+		String id = req.getParameter("boardID");
+		String subject = req.getParameter("subject");
+		String content = req.getParameter("content");
+		String category = req.getParameter("category");
+		String location = req.getParameter("location");
+		String inputMeetDate = req.getParameter("meetDate");
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime meetDate = LocalDateTime.parse(inputMeetDate, formatter);
+
+		BoardDTO boardDTO = BoardDTO.builder()
+			.email(userEmail)
+			.subject(subject)
+			.content(content)
+			.viewOption(1)
+			.category(category)
+			.location(location)
+			.meetDate(meetDate)
+			.build();
+
+		BoardService boardService = new BoardService();
+
+		try {
+
+			String writeMessage;
+
+			if (Objects.nonNull(id) && !id.isBlank()) {
+				boardDTO.setId(Long.parseLong(id));
+				boardService.modify(boardDTO);
+				writeMessage = "게시글 수정에 성공했습니다.";
+
+			} else {
+				boardService.write(boardDTO);
+				writeMessage = "게시글 작성에 성공했습니다.";
+			}
+
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			PrintWriter writer = resp.getWriter();
+			writer.println("{\"writeMessage\": \"" + writeMessage + "\"}");
+			writer.flush();
+
+		} catch (IllegalStateException illegalStateException) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, illegalStateException.getMessage());
+		}
+
 	}
 }
