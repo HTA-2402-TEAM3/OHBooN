5aeb329cb2a08778bf6fb34aa7ee2d49ff8838a1
keyy1315 <lovelina1315@gmail.com>
2024-06-13 17:32:08 +0900

    feat: Chat Receiver 채팅방 구현
    receiver ws 연결 이전 메세지 내역 불러오기,
    chat_id로 채팅방 연결

--- src/main/java/com/ohboon/ohboon/controller/ChatController.java
+++ src/main/java/com/ohboon/ohboon/controller/ChatController.java
@@ -1,9 +1,24 @@
 package com.ohboon.ohboon.controller;
 
-
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet("/startChat")
-public class ChatController {
+public class ChatController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long chat_id = Long.parseLong(req.getParameter("chat_id"));
 
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
