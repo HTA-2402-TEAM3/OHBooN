7a2219ec7b005d635697f31fa18824ba135111f8
HMYOON <yhm1992@naver.com>
2024-06-13 14:19:44 +0900

    feat : LoginFilter 수정 / err

--- src/main/java/com/jhta2402/jhta2402_3_team_project_1/filter/LoginFilter.java
+++ src/main/java/com/jhta2402/jhta2402_3_team_project_1/filter/LoginFilter.java
@@ -33,17 +33,17 @@ public class LoginFilter implements Filter {
             // 모달 메시지 생성 및 전달
             ModalDto modalDto = new ModalDto("알림", "게시판 입장을 위해 로그인을 해주세요.", "show");
             req.setAttribute("modal", modalDto);
-            req.getRequestDispatcher("/WEB-INF/member/login.jsp").forward(req, response);
+            req.getRequestDispatcher("/WEB-INF/user/login.jsp").forward(req, response);
 
 //            HttpServletResponse resp = (HttpServletResponse) response;
-//            resp.sendRedirect("../member/login");
+//            resp.sendRedirect("../user/login");
 
-            //ScriptWriter.alertAndNext(resp, "로그인 페이지로 이동합니다.", req.getContextPath() + "/member/login");
+            //ScriptWriter.alertAndNext(resp, "로그인 페이지로 이동합니다.", req.getContextPath() + "/user/login");
         }
     }
 }
 
-@WebFilter( {"/member/login/*"} )
+@WebFilter( {"/user/login/*"} )
 class LoginFilter2 implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
