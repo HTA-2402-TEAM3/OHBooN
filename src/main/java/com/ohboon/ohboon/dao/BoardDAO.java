8fa3c22b843b049bb02cccc810d1e597670dd7db
keyy1315 <lovelina1315@gmail.com>
2024-06-13 17:16:53 +0900

    Chore: Board xml, dao, service
    findEmailByBoard 메서드로 인한 추가

--- src/main/java/com/ohboon/ohboon/dao/BoardDAO.java
+++ src/main/java/com/ohboon/ohboon/dao/BoardDAO.java
@@ -1,4 +1,14 @@
 package com.ohboon.ohboon.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import util.MybatisConnectionFactory;
+
 public class BoardDAO {
+    public String findEmailByBoardId(long id){
+        SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
+        String userEmail = sqlSession.selectOne("findEmailByBoardId", id);
+        sqlSession.commit();
+        sqlSession.close();
+        return userEmail;
+    }
 }
--- src/main/java/com/ohboon/ohboon/service/BoardService.java
+++ src/main/java/com/ohboon/ohboon/service/BoardService.java
@@ -1,4 +1,14 @@
 package com.ohboon.ohboon.service;
 
+import org.apache.ibatis.session.SqlSession;
+import util.MybatisConnectionFactory;
+
 public class BoardService {
+    public String findEmailByBoardId(long id){
+        SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
+        String userEmail = sqlSession.selectOne("findEmailByBoardId", id);
+        sqlSession.commit();
+        sqlSession.close();
+        return userEmail;
+    }
 }
