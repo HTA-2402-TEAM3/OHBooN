1a802e10817c3506a013f45c208d5dbc333069ca
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-06-14 14:08:06 +0900

    Feat: 게시글 삭제 -> 게시글을 삭제하는 요청을 받고, 삭제하는 기능 구현
    게시글을 삭제하는 기능 구현

---
+++ src/main/java/com/ohboon/ohboon/controller/board/DeleteController.java
@@ -0,0 +1,25 @@
+package com.ohboon.ohboon.controller.board;
+
+import java.io.IOException;
+
+import com.ohboon.ohboon.service.BoardService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/delete")
+public class DeleteController extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		long boardID = Long.parseLong(req.getParameter("boardID"));
+
+		BoardService boardService = new BoardService();
+		boardService.delete(boardID);
+
+		resp.sendRedirect("/board/list");
+	}
+}
