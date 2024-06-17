82eb7df153f2e68de99a9ceae6cf38f3d0a7c032
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-06-17 14:05:00 +0900

    Feat: 게시글 검색 -> 검색 옵션에 따라 보여줄 게시글 목록을 조율하는 기능 구현
    간편 검색, 입력한 키워드로 검색 옵션에 따라 보여줄 게시글 목록을 조율하는 기능 구현

--- src/main/java/com/ohboon/ohboon/controller/board/ListController.java
+++ src/main/java/com/ohboon/ohboon/controller/board/ListController.java
@@ -1,9 +1,15 @@
 package com.ohboon.ohboon.controller.board;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
-import com.ohboon.ohboon.dto.BoardDTO;
+import com.ohboon.ohboon.dto.ListBoardDTO;
 import com.ohboon.ohboon.service.BoardService;
 
 import jakarta.servlet.RequestDispatcher;
@@ -19,6 +25,11 @@ public class ListController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String strPage = req.getParameter("page");
+		String searchOption = req.getParameter("searchOption");
+		String searchWord = req.getParameter("searchWord");
+		String location = req.getParameter("location");
+		String strMeetDate = req.getParameter("meetDate");
+		String category = req.getParameter("category");
 
 		int page = 0;
 		if (strPage != null && !strPage.isBlank()) {
@@ -29,7 +40,7 @@ public class ListController extends HttpServlet {
 
 		int totalPage = totalCount / 10;
 
-		if(totalCount > 10 && totalCount % 10 != 0) {
+		if (totalCount > 10 && totalCount % 10 != 0) {
 			totalPage++;
 		}
 
@@ -41,7 +52,35 @@ public class ListController extends HttpServlet {
 
 		int startIndex = page * 10 + 1;
 		int endIndex = startIndex + 9;
-		List<BoardDTO> boards = boardService.read(startIndex, endIndex);
+
+		HashMap<String, String> searchOptions = new HashMap<>(Map.of(
+			"searchOption", "",
+			"searchWord", "",
+			"startIndex", Objects.toString(startIndex),
+			"endIndex", Objects.toString(endIndex)));
+
+		List<ListBoardDTO> boards = new ArrayList<>();
+
+		if (Objects.nonNull(searchOption)) {
+			searchOptions.put("searchOption", searchOption);
+			searchOptions.put("searchWord", searchWord);
+			boards = boardService.search(searchOptions);
+		}
+
+		if (Objects.nonNull(strMeetDate)) {
+
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			LocalDateTime searchMeetDate = LocalDateTime.parse(strMeetDate, formatter);
+			LocalDateTime minTime = LocalDateTime.parse(strMeetDate, formatter).minusHours(2);
+			LocalDateTime maxTime = LocalDateTime.parse(strMeetDate, formatter).plusHours(2);
+
+			searchOptions.put("location", location);
+			searchOptions.put("minTime", minTime.format(formatter));
+			searchOptions.put("maxTime", maxTime.format(formatter));
+			searchOptions.put("category", category);
+
+			boards = boardService.search(searchOptions, searchMeetDate);
+		}
 
 		System.out.println(boards);
 
