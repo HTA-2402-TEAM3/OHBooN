3de2c57d6512d1b53797abbbda1dfea7445c3c9f
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-06-17 14:02:31 +0900

    Feat: 게시글 검색 -> 게시글 목록에서 보여줄 데이터를 담을 DTO 구현

---
+++ src/main/java/com/ohboon/ohboon/dto/ListBoardDTO.java
@@ -0,0 +1,26 @@
+package com.ohboon.ohboon.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ListBoardDTO {
+	private long id;
+	private String nickName;
+	private int evaluation;
+	private LocalDateTime regDate;
+	private String category;
+	private String subject;
+	private LocalDateTime meetDate;
+}
