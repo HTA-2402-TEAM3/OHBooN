57639d5b347f6f19d702ade60cf0e72f0224c1b1
keyy1315 <lovelina1315@gmail.com>
2024-06-18 09:26:28 +0900

    Chore: matching 테이블 -> sender 컬럼 추가
    게시글 한 개당 하나의 matching 튜플만 존재하는 문제 해결 위해 sender 속성 추가

--- src/main/java/com/ohboon/ohboon/service/MatchService.java
+++ src/main/java/com/ohboon/ohboon/service/MatchService.java
@@ -7,16 +7,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MatchService {
-    public long getMatchId(long board_id, String boardWriterName) {
+    public long getMatchId(long board_id, String boardWriterName, String senderName) {
         Map<String, Object> map = new HashMap<>();
         map.put("board_id", board_id);
         map.put("boardWriterName", boardWriterName);
+        map.put("senderName", senderName);
+
+        MatchDAO matchCreateDAO = new MatchDAO();
+        int rs = matchCreateDAO.createMatch(map);
 
         long match_id = 0;
 
+        Map<String, Object> map0 = new HashMap<>();
+        map0.put("board_id", board_id);
+        map0.put("senderName", senderName);
+
         MatchDAO matchDAO = new MatchDAO();
-        if(matchDAO.createMatch(map) > 0) {
-            match_id = matchDAO.getMatchId(map);
+        if(rs > 0) {
+            match_id = matchDAO.getMatchId(map0);
         }
         return match_id;
     }
