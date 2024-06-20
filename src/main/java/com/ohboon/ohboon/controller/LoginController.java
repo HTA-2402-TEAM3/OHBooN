cfe9a5eaeab4dce8f56879636b93791045480d58
keyy1315 <lovelina1315@gmail.com>
2024-06-20 10:32:16 +0900

    Chore : 채팅방 목록 -> 기존 request dispatcher 삭제

--- src/main/java/com/ohboon/ohboon/controller/LoginController.java
+++ src/main/java/com/ohboon/ohboon/controller/LoginController.java
@@ -29,8 +29,8 @@ public class LoginController extends HttpServlet {
 //      ChatRoomDAO의 getChatList() 메서드를 호출하여 현재 세션의 ID를 전달
 
         System.out.println(chatList);
-        req.setAttribute("chatList", chatList);
+//        req.setAttribute("chatList", chatList);
         ss.setAttribute("sessionNickname", userID);
-        req.getRequestDispatcher("chatList.jsp").forward(req,resp);
+        req.getRequestDispatcher("Test2.jsp").forward(req,resp);
     }
 }
